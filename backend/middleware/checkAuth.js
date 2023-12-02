5f1fb4b663dabf451bf874bb4709676501658e51
Omar Asael Hernandez Montiel <51717542+omarasael1980@users.noreply.github.com>
2023-12-01 19:25:59 -0800

    actualizacion back

--- backend/middleware/checkAuth.js
+++ backend/middleware/checkAuth.js
@@ -9,12 +9,12 @@ const checkAuth = async(req, res, next)=>{
         // it receives "Bearer numbersToken" so we split to get the numbers
         token = req.headers.authorization.split(' ')[1]
         //console.log('check',token)
-        const decoded = jwt.verify(token, process.env.JWT_SECRET)
+        const decoded = jwt.verify(token, process.env.JWT_SECRET) 
         //console.log(decoded)
         req.user = await Users.findById(decoded.id).select(
            '-password -accountStatus -createdAt -updatedAt -token -__v') 
        
-          //  console.log(req.user)
+          //  console.log(req.user) 
             return next() 
          
      } catch (error) {
